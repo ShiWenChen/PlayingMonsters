6de26d47b0c44b0339acd64b25c88ce1827a4e39
Chenshiwen <cswzmg@live.com>
2017-10-17 12:05:41 +0800

    Initial Commit

---
+++ PlayingMonsters/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PlayingMonsters
+//
+//  Created by ShiWen on 2017/10/17.
+//  Copyright © 2017年 ShiWen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PlayingMonsters/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PlayingMonsters
+//
+//  Created by ShiWen on 2017/10/17.
+//  Copyright © 2017年 ShiWen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
